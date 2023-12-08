4c88326a72000de08f1123603f186d1f27c76c85
geffrys <geffry.ospina@gmail.com>
2023-12-08 17:21:28 -0500

    RETO2: completado.

---
+++ retos/reto2.ts
@@ -0,0 +1,23 @@
+
+function manufacture(gifts:string[], materials:string):string[]{
+    let canManufacture:string[] = []
+    let flag:boolean = true;
+    gifts.forEach((gift)=>{
+        flag = true;
+        [...gift].forEach((m)=>{
+            if(materials.includes(m) == false){
+                flag = false;
+            }
+        });
+        if(flag == true){
+            canManufacture.push(gift);
+        }
+    })
+    return canManufacture;
+}
+
+
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+manufacture(gifts, materials)
